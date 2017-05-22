14611ba880ba48773252ae2e75998fbfda4b616b
martyni <martynjamespratt@gmail.com>
2017-05-22 13:44:55 +0000

    requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
+from pip.req import parse_requirements
 import app
+
+install_reqs = parse_requirements('requirements.txt')
+
+reqs = [ str(ir.req) for i in install_reqs ]
+
 setup(name='app',
       version=app.__version__,
       description='app',
@@ -7,6 +13,7 @@ setup(name='app',
       author='martyni',
       author_email='martynjamespratt@gmail.com',
       license='MIT',
+      install_requires=reqs,
       packages=['app'],
       zip_safe=False,
       entry_points = {
