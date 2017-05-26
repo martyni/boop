6b468439b0791bee0f4dc36489d5b8ea259a3973
martyni <martynjamespratt@gmail.com>
2017-05-26 16:29:51 +0000

    fixed?

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,5 +1,5 @@
 source env/bin/activate
-yes | zappa undeploy $1
-zappa deploy $1 2>&1 | tee build.log
+build=$(zappa update $1 2&>1 || zappa deploy $1 2&>1)
+echo $build | tee build.log
 awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
 cat url
