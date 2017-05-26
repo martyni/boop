6f288a099f8546b47e0eb66a4898dd45a438ade4
martyni <martynjamespratt@gmail.com>
2017-05-26 09:23:49 +0000

    fixed test

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,20 +1,20 @@
 source env/bin/activate
 boop &
 sleep 1
-failed = 0
+failed=0
 if [ "$(curl localhost:5000/test)" == "OMG" ]
   then  
      echo success
   else
      echo failure
-     failed = 1
+     failed=1
 fi
 if [ "$(curl localhost:5000/)" == "file" ]
   then  
      echo success
   else
      echo failure
-     failed = 1
+     failed=1
 fi
 export PID=$(ps aux | grep -v awk |awk /boop/'{print $2}')
 echo killing $PID
