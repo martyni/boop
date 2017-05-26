183017417217f3662353b66c009c55b32b64f5dc
martyni <martynjamespratt@gmail.com>
2017-05-26 09:20:53 +0000

    useful app

--- app/application.py
+++ app/application.py
@@ -1,9 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+import boto3
 
+app = Flask(__name__)
+client = boto3.client("s3")
 @app.route('/test')
 def test():
    return 'OMG'
+
+@app.route('/')
+def list_files():
+    blob = client.list_objects_v2(Bucket="martyni-boop")
+    contents = blob['Contents']
+    print contents
+    return [f['Key'] for f in contents][0]
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,16 +1,22 @@
 source env/bin/activate
 boop &
 sleep 1
+failed = 0
 if [ "$(curl localhost:5000/test)" == "OMG" ]
   then  
      echo success
-     EXIT=0
   else
      echo failure
-     EXIT=1
-     
+     failed = 1
+fi
+if [ "$(curl localhost:5000/)" == "file" ]
+  then  
+     echo success
+  else
+     echo failure
+     failed = 1
 fi
 export PID=$(ps aux | grep -v awk |awk /boop/'{print $2}')
 echo killing $PID
 echo $PID >/tmp/PID
-echo $EXIT >/tmp/EXIT
+echo $failed >/tmp/EXIT
