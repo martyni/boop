7880b5548ef929aa9076f7b9fa78998c66b6b8de
martyni <martynjamespratt@gmail.com>
2017-05-26 14:39:32 +0000

    fixed awk command

--- jenkins/deploy_dev.sh
+++ jenkins/deploy_dev.sh
@@ -1,4 +1,4 @@
 source env/bin/activate
 zappa update dev >build.log
-awk -F "live!:"/amazonaws.com/'{print $2}' build.log > url
+awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
 cat url
