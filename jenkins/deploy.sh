55e2e89be3767559f5e365cead1db9b50b039b38
martyni <martynjamespratt@gmail.com>
2017-05-26 15:24:38 +0000

    grab url better

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,4 +1,5 @@
 source env/bin/activate
-zappa update $1 || zappa deploy $1>build.log
+build=$(zappa update $1 || zappa deploy $1)
+echo $build >build.log
 awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
 cat url
