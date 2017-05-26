6751f720603eaa014c3c34aea759a98b5924fe58
martyni <martynjamespratt@gmail.com>
2017-05-26 16:14:26 +0000

    trying to fix

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,5 +1,5 @@
 source env/bin/activate
-build=$(zappa update $1 || zappa deploy $1)
+build=$(zappa deploy $1)
 echo $build >build.log
 awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
 cat url
