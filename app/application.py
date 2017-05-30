04845283ed683d7c41ccff3ce6fe3930a583ac09
martyni <martynjamespratt@gmail.com>
2017-05-30 09:24:55 +0000

    hacked url 4 to remove prod string to work with cloudfront

--- app/application.py
+++ app/application.py
@@ -4,6 +4,9 @@ import boto3
 app = Flask(__name__)
 client = boto3.client("s3")
 
+def url_4(*args, **qwargs):
+   return url_for(*args, **qwargs).replace('/prod', '')
+
 def get_files(Bucket="martyni-boop", path="", folders=False):
     blob = client.list_objects_v2(Bucket="martyni-boop")
     contents = blob['Contents']
@@ -18,7 +21,7 @@ def test():
 
 @app.route('/')
 def list_files():
-    return ''.join(['<a href="{url}">{series}</a>'.format(url=url_for('series', name=folder), series=folder) for folder in get_files(folders=True)])
+    return ''.join(['<a href="{url}">{series}</a>'.format(url=url_4('series', name=folder), series=folder) for folder in get_files(folders=True)])
 
 @app.route('/series/<name>')
 def series(name):
