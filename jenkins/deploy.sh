f1d1ce90456fe6709987790d7a3e95f6927d29cb
martyni <martynjamespratt@gmail.com>
2017-06-05 15:43:23 +0000

    updated deploy for more generic rerunning

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,4 +1,7 @@
 source env/bin/activate
 zappa update $1 | tee build.log
 awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
-cat url
+if [ -z $(cat url) ] 
+    zappa deploy $1 | tee build.log
+    awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
+fi    
