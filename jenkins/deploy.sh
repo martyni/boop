905501324f907caafcea57b2586aa19b018c8983
martyni <martynjamespratt@gmail.com>
2017-06-05 15:44:47 +0000

    bash...

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,6 +2,7 @@ source env/bin/activate
 zappa update $1 | tee build.log
 awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
 if [ -z $(cat url) ] 
+    then
     zappa deploy $1 | tee build.log
     awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
 fi    
